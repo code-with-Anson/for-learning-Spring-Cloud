6e80ddba63735aca6d033f017146dd5a1d3c2128
mocha <2795757650@qq.com>
2024-12-15 17:42:58 +0800

    完成了把订单模块拆分成微服务，同时明确了之后把所有的调用需求使用hm-api的client来进行发送请求
    一般来讲，应用feign有两种方式
    第一种是在每个微服务模块里面专门去定义一些client来发送请求，并且在里面放置DTO等必要实体类，唯一的缺点就是项目结构会复杂一些，但是是遵守微服务原则的
    
    第二种是抽象出一个类似hm-api这样的模块，因为这个项目是用来本地开发学习的，所以我们使用了这样的方法，但是！会造成耦合度高！这是违背微服务原则的

--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -2,11 +2,11 @@ package com.hmall.item.controller;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import com.hmall.common.domain.PageDTO;
 import com.hmall.common.domain.PageQuery;
 import com.hmall.common.utils.BeanUtils;
-import com.hmall.item.domain.dto.ItemDTO;
-import com.hmall.item.domain.dto.OrderDetailDTO;
 import com.hmall.item.domain.po.Item;
 import com.hmall.item.service.IItemService;
 import io.swagger.annotations.Api;
@@ -35,7 +35,7 @@ public class ItemController {
 
     @ApiOperation("根据id批量查询商品")
     @GetMapping
-    public List<ItemDTO> queryItemByIds(@RequestParam("ids") List<Long> ids){
+    public List<ItemDTO> queryItemByIds(@RequestParam("ids") List<Long> ids) {
         return itemService.queryItemByIds(ids);
     }
 
@@ -54,7 +54,7 @@ public class ItemController {
 
     @ApiOperation("更新商品状态")
     @PutMapping("/status/{id}/{status}")
-    public void updateItemStatus(@PathVariable("id") Long id, @PathVariable("status") Integer status){
+    public void updateItemStatus(@PathVariable("id") Long id, @PathVariable("status") Integer status) {
         Item item = new Item();
         item.setId(id);
         item.setStatus(status);
@@ -78,7 +78,7 @@ public class ItemController {
 
     @ApiOperation("批量扣减库存")
     @PutMapping("/stock/deduct")
-    public void deductStock(@RequestBody List<OrderDetailDTO> items){
+    public void deductStock(@RequestBody List<OrderDetailDTO> items) {
         itemService.deductStock(items);
     }
 }
